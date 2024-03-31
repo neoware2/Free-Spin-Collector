415ee55425776e8eadab3562d3b0241872ae6421
dabllord <dablvin767@gmail.com>
2024-03-31 22:52:57 +0300

    Login Mozzart App

---
+++ script.py
@@ -0,0 +1,37 @@
+import argparse
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-u","--username", help="Username for Mozzart Bet", type=str, default="")
+parser.add_argument("-p","--password", help="Password for Mozzart Bet", type=str, default="")
+
+args = parser.parse_args()
+
+username, password = args.username, args.password
+
+driver = webdriver.Chrome()
+driver.get("https://www.mozzartbet.com/sr#/mozz-app?mozzAppConversation=64e5a8ae85ad0bdcc91e36eb")
+
+driver.find_element(By.XPATH, '//*[@id="gdpr-wrapper-new"]/div/div[3]/div[2]/button').click()
+sleep(5)
+
+driver.find_element(By.XPATH, '//*[@id="onesignal-slidedown-cancel-button"]').click()
+
+driver.find_element(By.CSS_SELECTOR, '#pageWrapper > div > header > section.nav-top > article.nav-group.min-width-400.user-part.mozzart_sr > section > section > div > div.bottom-part > span').click()
+
+driver.find_element(By.XPATH, '//*[@id="pageWrapper"]/section/div/p[4]').click()
+
+sleep(3)
+
+driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(username)
+driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(password)
+
+driver.find_element(By.CSS_SELECTOR, '#pageWrapper > div > header > section.nav-top > article.nav-group.min-width-400.user-part.mozzart_sr > section > article > div:nth-child(2) > div > div > div > div.modal__body > form > button').click()
+
+driver.find_element(By.XPATH, '//*[@id="pageWrapper"]/section/div/div/div/div/div[1]/div/div/div/div[3]').click()
+
+breakpoint()
